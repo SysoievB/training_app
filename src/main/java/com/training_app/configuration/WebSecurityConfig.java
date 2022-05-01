d04f4074b60e111643a5d20f6f6950476cb537f3
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-05-01 12:48:41 +0200

    added roles spring security

--- src/main/java/com/training_app/configuration/WebSecurityConfig.java
+++ src/main/java/com/training_app/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.training_app.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
